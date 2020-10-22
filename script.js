39d918835bdb826628c1d6ee6c54a5b96d6dfe37
Maria <mrzvmaria@yandex.ru>
2020-10-22 15:36:58 +0300

    zero instead of negative numbers

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //1) setting the end point of the timer:
-let deadline = '2020-10-24';
+let deadline = '2018-10-24';
 
 //2) founding out how much time is remained to the end point of the timer:
 function getTimeRemaining(endpoint) {
@@ -30,13 +30,24 @@ function setClock(id, endpoint) {
 
     function updateClock() {
         let updatedTime = getTimeRemaining(endpoint);
-        days.textContent = updatedTime.days;
-        hours.textContent = updatedTime.hours;
-        minutes.textContent = updatedTime.minutes;
-        seconds.textContent = updatedTime.seconds;
+
+        function addZero(num) {
+            if (num <= 9) {
+                return '0' + num;
+            } else return num;
+        };
+
+        days.textContent = addZero(updatedTime.days);
+        hours.textContent = addZero(updatedTime.hours);
+        minutes.textContent = addZero(updatedTime.minutes);
+        seconds.textContent = addZero(updatedTime.seconds);
 
         if (updatedTime.total <= 0) {
             clearInterval(timeInterval);
+            days.textContent = '00';
+            hours.textContent = '00';
+            minutes.textContent = '00';
+            seconds.textContent = '00';
         }
     }
 }
